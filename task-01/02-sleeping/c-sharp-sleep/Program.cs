694a65eaa1c4f3357aa8273b6e4932ee294101a2
Lidiia Makarchuk <87641510+norilanda@users.noreply.github.com>
2026-03-13 16:32:10 +0100

    Add 01 sleeping task

---
+++ task-01/02-sleeping/c-sharp-sleep/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+int defaultIterations = 400;
+int defaultSleepMs = 1;
+
+RunSleepTest(defaultIterations, defaultSleepMs);
+
+static void RunSleepTest(int iterations, int sleepMs)
+{
+    int warmUpIterations = iterations;
+
+    long[] timeStamps = new long[iterations];
+    long[] warmUpTimeStamps = new long[warmUpIterations];
+    TimeSpan[] deltas = new TimeSpan[iterations - 1];
+    TimeSpan[] warmUpDeltas = new TimeSpan[warmUpIterations - 1];
+
+    // Warm-Up
+    for (int i = 0; i < warmUpIterations; i++)
+    {
+        warmUpTimeStamps[i] = Stopwatch.GetTimestamp();
+        Thread.Sleep(sleepMs);
+    }
+
+    // Actual Test
+    for (int i = 0; i < iterations; i++)
+    {
+        timeStamps[i] = Stopwatch.GetTimestamp();
+        Thread.Sleep(sleepMs);
+    }
+
+    // Calculate Deltas
+    for (int i = 0; i < iterations - 1; i++)
+    {
+        deltas[i] = Stopwatch.GetElapsedTime(timeStamps[0], timeStamps[i + 1]);
+        warmUpDeltas[i] = Stopwatch.GetElapsedTime(warmUpTimeStamps[0], warmUpTimeStamps[i + 1]);
+    }
+
+    var filePath = $"sleep-results-{sleepMs}ms.csv";
+    var filePathWarmUp = $"sleep-results-warmup-{sleepMs}ms.csv";
+
+    using (StreamWriter writer = new(filePath))
+    {
+        for (int i = 0; i < iterations - 1; ++i)
+        {
+            writer.WriteLine(deltas[i].TotalMilliseconds);
+        }
+    }
+
+
+    using (StreamWriter writer = new(filePathWarmUp))
+    {
+        for (int i = 0; i < iterations - 1; ++i)
+        {
+            writer.WriteLine(warmUpDeltas[i].TotalMilliseconds);
+        }
+    }
+}
